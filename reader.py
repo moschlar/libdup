eda63bda6b3e345014c07716d38feeddec3f254d
Christian Hundt <hundt.christian@googlemail.com>
2012-11-06 18:08:50 +0100

    added hist method

--- reader.py
+++ reader.py
@@ -2,33 +2,27 @@ import cv2
 import numpy as np
 import pylab as pl
 
+def hist(image):
+
+    histogram = np.zeros(256, dtype=np.int32)
+
+    for x in xrange(image.shape[0]):
+        for y in xrange(image.shape[1]):
+            histogram[image[x, y]] += 1
+    
+    return histogram
 
 def read(filename):
 
     capture = cv2.VideoCapture(filename)
 
-    while(True):
-        success, frame = capture.read()
-	frame = cv2.resize(frame, (128, 128))
-
-
-	pl.figure(1)
-	pl.title("red")
-	pl.hist(frame[:,:,0])	
-
-	pl.figure(2)
-	pl.title("green")
-	pl.hist(frame[:,:,1])	
-	
-	pl.figure(3)
-	pl.title("blue")
-	pl.hist(frame[:,:,2])	
-	
-	pl.figure(4)
-	pl.imshow(frame, interpolation="nearest")
-	pl.show()
-	
-
+    images = [cv2.resize(capture.read()[1], (32, 32)) for count in xrange(9)]
+    
+    print len(images)
+    
+    for color in xrange(3):
+        hists = [hist(images[count][:,:,color]) for count in xrange(9)]
+        print hists
 
 read("./data/legostein.mp4")
 
